6ab55c643aa5eb9de05158b29c536387f7d353c1
Xeei <109275765+Xeei@users.noreply.github.com>
2026-03-26 23:24:10 +0700

    chore: add config.py

---
+++ hw10_1/config.py
@@ -0,0 +1,9 @@
+# Database connection credentials
+DB_HOST = ""
+# Replace with your database host
+DB_USER = ""
+# Replace with your database username
+DB_PASSWD = ""
+# Replace with your database password
+DB_NAME = ""
+# Replace with your database name
